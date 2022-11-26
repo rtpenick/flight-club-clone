ebd12984a6014ed736bf56c5c9643a41fcadd9dd
rtpenick <tyreepenick@gmail.com>
2022-11-25 23:53:55 -0500

    adding more details to sneaker details

--- src/pages/Sneaker/components/SneakerDetails.js
+++ src/pages/Sneaker/components/SneakerDetails.js
@@ -2,26 +2,35 @@ import React from 'react'
 import SneakerSizes from './SneakerSizes'
 
 const SneakerDetails = ({setIsDrawerOpen,sneakerData}) => {
-
-    
   return (
-    <div>
-        <div className='Sneaker_name&brand'>
-                {/* component */}
-                <h2>{sneakerData.name}</h2>
-            </div>
-            <div className="Sneaker__sizes">
-                <SneakerSizes/>
-            </div>
-            <div className="Sneaker__buttons">
-                <button 
-                  type="button" 
-                  className='priceBtn' 
-                  onClick={() => setIsDrawerOpen(true)}
-                >
-                    ${sneakerData.price}
-                </button>
-                <button type="button">Out of Stock</button>
+    <>
+      <div className='Sneaker_name&brand'>
+        {/* component */}
+        <p className='Sneaker_rightContent_brand'>{sneakerData.brand}</p>
+        <h2 className='Sneaker_rightContent_title'>{sneakerData.name}</h2>
+      </div>
+      <div className="Sneaker__sizes">
+        <SneakerSizes sneakerData={sneakerData}/>
+      </div>
+            <div className="Sneaker_rightContent_btnSection">
+                <div className='Sneaker_rightContent_btnSection_buttons'>
+                    <div>
+                        <p>BUY NEW</p>
+                        <button 
+                        type="button" 
+                        className='priceBtn' 
+                        onClick={() => setIsDrawerOpen(true)}
+                        >
+                        ${sneakerData.price}
+                        </button>
+                    </div>
+                    <div>
+                        <p>BUY USED</p>
+                        <button type="button">Out of Stock</button>
+                    </div>
+                </div>
+                <p>Pay in 4 interest-free payments with Klarna, Affirm or Afterpay. </p>
+                <p>Shopping from </p>
             </div>
             <div className='Sneaker_aboutProduct'>
                 <h4>About This Product</h4>
@@ -31,9 +40,8 @@ const SneakerDetails = ({setIsDrawerOpen,sneakerData}) => {
             <div className='Sneaker_shipping'>
                 <h4>Shipping & Returns</h4>
                 {/* component */}
-
             </div>
-    </div>
+    </>
   )
 }
 
--- src/pages/Sneaker/components/SneakerSizes.js
+++ src/pages/Sneaker/components/SneakerSizes.js
@@ -1,23 +1,38 @@
 import React from 'react'
 
-const SneakerSizes = () => {
+const SneakerSizes = ({sneakerData}) => {
   return (
     <div>
-        <label className="option_item" htmlFor="size4">
-            <input type="radio" name="radio" id="size4"/>
-            <div className='option-inner'>
-                <div className='icon'></div>
-                <div className='tickmark'></div>
-            </div>
-        </label>
+        <p>SELECT US MENS</p>
 
-        <label className="option_item" htmlFor="size3">
-            <input type="radio" name="radio" id="size3"/>
-            <div className='option-inner'>
-                <div className='icon'></div>
-                <div className='tickmark'>This</div>
-            </div>
-        </label>
+        <div class="radios">
+            {/* {sneakerData.sizes.map(item => (
+                <>
+                <input type="radio" id="r1" name="rGroup"/>
+                <label class="radio" for="r1" name="rGroup">{item}</label>
+                </>
+            ))} */}
+            <input type="radio" id="r1" name="rGroup"/>
+            <label class="radio" for="r1" name="rGroup">8</label>
+
+            <input type="radio" id="r2" name="rGroup"/>
+            <label class="radio" for="r2" name="rGroup">8.5</label>
+
+            <input type="radio" name="rGroup" value="3" id="r3" />
+            <label class="radio" for="r3">9</label>
+
+            <input type="radio" name="rGroup" value="3" id="r4" />
+            <label class="radio" for="r4">9.5</label>
+            
+            <input type="radio" name="rGroup" value="3" id="r5" />
+            <label class="radio" for="r5">10</label>
+
+            <input type="radio" name="rGroup" value="3" id="r6" />
+            <label class="radio" for="r6">10.5</label>
+            
+            <input type="radio" name="rGroup" value="3" id="r7" />
+            <label class="radio" for="r7">11</label>
+        </div>
     </div>
   )
 }
